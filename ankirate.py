ff7804207fde27123e4c902954c4045a9c90445c
deedstho <deedstho@umich.edu>
2016-12-24 15:29:32 -0500

    created empty

---
+++ ankirate.py
@@ -0,0 +1,10 @@
+# import the main window object (mw) from aqt
+from aqt import mw
+
+#import the "show info" tool from utils.py
+from aqt.utils import showInfo
+
+# import all of the Qt GUI library
+from aqt.qt import *
+
+
